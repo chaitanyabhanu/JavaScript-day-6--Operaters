1e21cc12083dd9ed819bb80d7f314ffb6c7bd0b1
Bhanu Chaitanya <143734650+chaitanyabhanu@users.noreply.github.com>
2024-08-03 13:54:15 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,54 @@
+// JavaScript Operators
+
+//   JavaScript Arithmetic Operators 
+// Arithmetic operators perform arithmetic on numbers (literals or variables)
+
+// (+) Addition
+let a= 1 ;
+let b= 0;
+console.log (a+ b );
+
+// (-) Subtraction
+
+let c=10;
+let d=8;
+console.log(c-d);
+
+// ( * ) -- Multiplications
+
+let e=3;
+let f=1;
+console.log(e*f);
+
+// (**) - Exponentiation (ES2016)
+
+let h=5;
+let g=1;
+console.log(h**g);
+
+//  (/)  Division 
+
+let i=6;
+let j=1;
+console.log(i/j);
+
+//  (%) Modulus (Division Remainder)
+
+let k=7;
+let p = 16;
+console.log(k%p);
+
+ // (++) Increment  
+
+let m=1;
+let n=7;
+console.log(m + + n);
+
+// (--) Decrement  
+let x=10;
+console.log(--x);
+
+
+
+
+
